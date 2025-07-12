be2b8f56839aa001afd0afdf7e469f975bb66614
Kuro0911 <dhananjaisharma09@gmail.com>
2025-07-12 13:03:04 +0800

    minor fix

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ import random
 # global
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 LORA_SCALE = 1
-SEEDS = [random.randint(0, 2**32) for _ in range(NUM_IMAGES_PER_EVAL)]
 
 IMAGE_MODEL = "sd-legacy/stable-diffusion-v1-5"
 CLIP_MODEL = "openai/clip-vit-large-patch14"
@@ -23,6 +22,7 @@ POPULATION_SIZE = 16
 MAX_PROMPT_LENGTH = 8
 SIMILARITY_TOP_N = 3000
 NUM_IMAGES_PER_EVAL = 10
+SEEDS = [random.randint(0, 2**32) for _ in range(NUM_IMAGES_PER_EVAL)]
 
 NUM_INFERENCE_STEPS = 25
 
